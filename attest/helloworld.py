39e0ecaf39ff547585b93926bd6b351adac2c43d
nathro <nathro.software@gmail.com>
2022-05-04 18:44:54 -0700

    Set up initial Repo

---
+++ attest/helloworld.py
@@ -0,0 +1,12 @@
+# @black_format
+
+class HelloWorld:
+    def print_hello():
+        print("Hello World!")
+    def print_goodbye():
+        print("Goodbye!")
+
+if __name__ == "__main__":
+    world = HelloWorld()
+    world.print_hello()
+    world.print_goodbye()
