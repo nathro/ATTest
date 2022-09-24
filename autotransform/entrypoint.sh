cbb5d0c48ec9a0ade737973080dd8fa9765af760
nathro <nathro.software@gmail.com>
2022-09-23 20:36:40 -0700

    Docker ready for testing

--- autotransform/entrypoint.sh
+++ autotransform/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-docker build --build-arg REPO_DIR=$REPO_DIR \
+docker build -f autotransform/Dockerfile \
+    --build-arg REPO_DIR=$REPO_DIR \
     --build-arg COMMAND=$COMMAND \
     -t autotransform \
+    --no-cache \
     autotransform
 
 if [[ $COMMAND == "schedule" || $COMMAND == "manage" ]]; then
